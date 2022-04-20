a929536d9d3ef678becdc9eae9f311e64f7a1fe2
khalidhersi <cipherpolfoundation@gmail.com>
2022-04-20 19:09:51 +0100

    working on player movement

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 //object positions
-let playerX = 220;
-let playerY = 120;
+var playerHeight = 50;
+var playerWidth = 50;
+let playerX = (canvas.width-playerWidth)/2;;
+let playerY = (canvas.height-playerHeight)/2;;
+// let playerX = 220;
+// let playerY = 120;
 let playerDX = 1;
 let playerDY = 1;
 let greeBallX = 50;
@@ -16,8 +20,55 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var upPressed = false;
+var downPressed = false;
+var leftPressed = false;
+var rightPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Up" || e.key == "ArrowUp" || "w") {
+        upPressed = true;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown" || "s") {
+        downPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft" || "a") {
+        leftPressed = true;
+    }
+    else if(e.key == "Right" || e.key == "ArrowRight" || "d") {
+        rightPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Up" || e.key == "ArrowUp" || "w") {
+        upPressed = false;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown" || "s") {
+        downPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft" || "a") {
+        leftPressed = false;
+    }
+    else if(e.key == "Right" || e.key == "ArrowRight" || "d") {
+        rightPressed = false;
+    }
+}
 
 function drawPlayer() {
+    ctx.beginPath();
+    ctx.rect(playerX, playerY, playerWidth, playerHeight);    
+    ctx.fillStyle = "#6867d3";
+    ctx.fill();
+    ctx.closePath();
+    // playerX += playerDX
+    // playerY += playerDY 
+}
+
+function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#FF0000";
@@ -27,6 +78,7 @@ function drawPlayer() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
     drawPlayer();
      
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -34,6 +86,31 @@ function draw() {
     }
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
+    }
+
+    if(upPressed) {
+        playerY -= 2;
+        if (playerY < 0){
+            playerY = 0;
+        }
+    }
+    else if(downPressed) {
+        playerY += 2;
+        if (playerY + playerHeight > canvas.height){
+            playerY= canvas.height - playerHeight;
+        }
+    }
+    else if(leftPressed) {
+        playerX -= 2;
+        if (playerX < 0){
+            playerX = 0;
+        }
+    }
+    else if(rightPressed) {
+        playerX += 2;
+        if (playerX + playerWidth > canvas.width){
+            playerX = canvas.width - playerWidth;
+        }
     }
     
     x += dx;
